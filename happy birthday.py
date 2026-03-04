5ef215ef27e5f91d21668436e1006d5c3b897d64
kaylebb26 <kdanquahansah@gmail.com>
2026-03-04 20:24:15 +0000

    Add files via upload

---
+++ happy birthday.py
@@ -0,0 +1,8 @@
+def happy_birthday(name):
+    print("Happy Birthday to you!")
+    print("Happy Birthday to you!")
+    print("Happy Birthday dear kayleb!")
+    print("Happy Birthday to you!")
+
+
+happy_birthday("kayleb")
